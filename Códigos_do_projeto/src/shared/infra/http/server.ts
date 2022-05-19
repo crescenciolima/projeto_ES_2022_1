6e035377854bb9ab429f7022e2c04cb18b86b83d
Brendon Sousa Lima <bredbk@gmail.com>
2022-05-19 02:21:58 -0300

    alterando arquivo ormconfig.json e detalhando porta do server

--- Códigos_do_projeto/src/shared/infra/http/server.ts
+++ Códigos_do_projeto/src/shared/infra/http/server.ts
@@ -1,3 +1,5 @@
 import { app } from "./app";
 
-app.listen(3333, () => console.log("Server is running!"))
+const port = 3333;
+
+app.listen(port, () => console.log(`Server is running! on port ${port} visit http://localhost:3333`))
